eb0e2c6406c12f31713950b2e8851d31f57796e8
Gegardus <stgegard@gmail.com>
2022-08-12 12:03:00 +0400

    Add validations and associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :transactions
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :categories
+    has_many :transactions, through: :categories
+  
+    validates :name, :email, :password, presence: true
+  end
 end
