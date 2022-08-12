3eeb139bdd443ca20bc9fb9edfd25edc0c5f9734
Gegardus <stgegard@gmail.com>
2022-08-12 12:17:27 +0400

    Add controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  # get 'transactions/index'
+  # get 'transactions/show'
+  # get 'transactions/new'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+
+  resources :categories
+  resources :transactions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'categories#index'
 end
